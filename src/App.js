aab5882afb7d7de730fc79d77a803ab75fc3b1b5
pgb4 <preston.bebas1@gmail.com>
2020-03-17 14:00:13 -0400

    added redux and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import './App.css';
 
@@ -8,34 +9,26 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state={
-      currentUser: null
-    }
-  }
-
 
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const { setCurrentUser } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=> {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+            setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          })
+            })
         })
       } else {
-      this.setState({ currentUser: userAuth })  
+      setCurrentUser(userAuth)  
     }})
   }
 
@@ -47,7 +40,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route
             exact
@@ -69,4 +62,14 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+//null passed in for first argument which would have been mapStateToProps bc this component
+// doesn't need to know anything about the the app's state
+//it just needs to be able to make adjustments to it
+export default connect(
+  null, 
+  mapDispatchToProps
+  )(App);
